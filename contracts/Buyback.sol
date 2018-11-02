3a7d59756cd8d061b1343cfee88a0e632ba318a1
Omidiora Samuel <>
2018-11-02 13:27:27 +0100

    added deposit function

--- contracts/Buyback.sol
+++ contracts/Buyback.sol
@@ -6,10 +6,7 @@ import "@gnosis.pm/dx-contracts/contracts/Oracle/PriceOracleInterface.sol";
 
 /**
 * Set maximum an address can buy
-*
-*
-*
-*
+* approve and allow
 */
 
 contract BuyBack {
@@ -19,7 +16,15 @@ contract BuyBack {
     // SellToken the token that is sold
     address public sellToken;
     address public buyToken;
+
+    // mapping of the sell token to 
+    mapping (address => uint) public balances;
+
+    uint256[] auctionIDs;
+
+
     bool shouldBurnToken;
+
     // BuyToken the token that is bought
     DutchExchange public dx;
 
@@ -28,21 +33,21 @@ contract BuyBack {
         _;
     }
 
-    event SellTokensDeposit(
-        address indexed tokenAddress,
-        uint256 amount,
-        uint256 
+    event Withdraw(
+         address indexed token,
+         uint amount
     );
 
-    event BuyTokensDeposit (
-
+    event Deposit (
+        address indexed tokenAddress,
+        uint amount
     );
 
     event BuyBack (
         address indexed from,
         address indexed to,
-        uint256 amount,
-        uint256 token
+        uint amount,
+        uint token
     );
        
     /**
@@ -50,10 +55,12 @@ contract BuyBack {
      * @param _buyToken Address of the security token
      * @param _sellToken Address of the polytoken
      * @param _burn Should burn the token after buy back success
+     * @param 
      */
-    constructor(address _buyToken, address _sellToken, bool _burn) public {
+    constructor(address _buyToken, address _sellToken, bool _burn, uint256 _amount) public {
         sellToken = _sellToken;
         buyToken = _buyToken;
+        amount = _amount;
         shouldBurnToken = _burn;
         (pricetNum, priceDen) = dx.getPriceOfTokenInLastAuction(token);
     }
@@ -62,20 +69,31 @@ contract BuyBack {
         dx = _dx;
     }
 
+    function deposit (address token, uint amount) public onlyOwner returns (uint) {
+        require(amount > 0);
+        require(Token(token).transferFrom(msg.sender, this, amount));
+        
+        balances[token] += amount;
+        emit Deposit(token, amount);
+
+        return amount;
+    }
+
+    function _approve() {
+        _sellToken
+    }
+
     function transfer() {
+        // deposit would allow sell orders
+        // with token pair to the 
+        dx.addTokenPair()
+
 
     }
 
     function burnTokens() {
 
+
     }
-
-    function () payable {
-        emit BuyTokenDeposit(
-
-        );
-    }
-
-
 
 }
