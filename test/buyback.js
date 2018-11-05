756d376bffc00350e30ce2ce91e69291f1d5cee1
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2018-11-05 12:40:09 +0100

    changes

--- test/buyback.js
+++ test/buyback.js
@@ -5,7 +5,7 @@ const TokenGNO = artifacts.require('TokenGNO')
 contract("Buyback", accounts => {
     describe("Test Buyback Implementation", async() => {
         it("Create Buyback", async() => {
-
+            
         })
     })
 })
